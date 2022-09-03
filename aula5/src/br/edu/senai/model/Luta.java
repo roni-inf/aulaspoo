93fd3039ebda703071a9b36475568d2e6df74801
roni-inf <roni_inf@hotmail.com>
2022-09-03 08:09:04 -0300

    Aula 5

---
+++ aula5/src/br/edu/senai/model/Luta.java
@@ -0,0 +1,38 @@
+package br.edu.senai.model;
+
+public class Luta {
+	private Lutador desafiante;
+	private Lutador desafiado;
+	private Boolean confirmacao = false;
+
+	public Luta(Lutador desafiante, Lutador desafiado, Boolean confirmacao) {
+		super();
+		this.desafiante = desafiante;
+		this.desafiado = desafiado;
+		this.confirmacao = confirmacao;
+	}
+
+	public Lutador getDesafiante() {
+		return desafiante;
+	}
+
+	public Lutador getDesafiado() {
+		return desafiado;
+	}
+
+	public Boolean getConfirmacao() {
+		return confirmacao;
+	}
+
+	public Boolean confirmacaoLuta() {
+		// Para lutar ser confirmada é necessária checar se o
+		// desafiante é diferente do desafiado e também se as categorias
+		// são iguais
+
+		if (!desafiante.equals(desafiado) && desafiante.getCategoria().equals(desafiado.getCategoria())) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ aula5/src/br/edu/senai/model/PlanoDeSaude.java
@@ -0,0 +1,28 @@
+package br.edu.senai.model;
+
+public class PlanoDeSaude {
+	protected String empresa;
+	protected Double valorPago = 80.0;
+	protected Double valorISS = 5.0;
+
+	public PlanoDeSaude(String empresa) {
+		this.empresa = empresa;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public Double getValorPago() {
+		return valorPago;
+	}
+
+	public Double getValorISS() {
+		return valorISS;
+	}
+
+	public Double calcularPagamento() {
+		valorPago = valorPago - valorPago * valorISS / 100;
+		return valorPago;
+	}
+}
